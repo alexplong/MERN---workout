60cf0122e556064cd882e09d36ad87d06a8666a4
Alex Plong <alexplong@gmail.com>
2023-03-06 16:01:44 -0500

    - fetch all and post new workout added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 /** @format */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages & components
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,37 @@
+/** @format */
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  // import in all workouts
+  useEffect(() => {
+    // fetch workouts from api
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []); // empty array to only have effect hook run only once
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
+
+export default Home;
